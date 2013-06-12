2f55d6314f9b2ed14333c9ec29dbe9a542028aa3
boedy1996 <boedy1996@gmail.com>
2013-06-12 18:46:41 +0500

    Update dbconnect.php

--- sec/sec-m/dbconnect.php
+++ sec/sec-m/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 
 function getDB() {
-	return pg_connect("host=210.56.8.110 port=5433 dbname=securitynews user=postgres password=!MM@P2011");
+	return pg_connect("host=HOST port=PORT dbname=DBNAME user=postgres password=PASS");
 }
 ?>
